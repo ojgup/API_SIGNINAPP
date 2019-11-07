41377497bc1d84cf05a21339918d9e9649945efe
isaacblock <102561305@student.swin.edu.au>
2019-11-08 09:43:02 +1100

    Individual Classes
    A class file has been created for each class. (Attendence, Classes, Label, Session, Student)

---
+++ API_SIGNINAPP/Models/Attendence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_SIGNINAPP.Models
+{
+    public class Attendence
+    {
+        public int Barcode { get; set; }
+        public string Date { get; set; }
+        public int ClassID { get; set; }
+        public string Time { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public bool actionPerformed { get; set; }
+
+        public Attendence(int barcode, string date, int classID, string time, double longitude, double latitude, bool actionperformed)
+        {
+
+        }
+
+    }
+}
